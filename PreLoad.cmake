9fcf98f13b7aef15f1c5d7ec9abac4bce9fe5fe6
Likan <ChaosDev@seznam.cz>
2013-03-02 17:40:16 +0100

    loader

---
+++ PreLoad.cmake
@@ -0,0 +1,29 @@
+# 
+# Copyright (C) Likan Mod   (https://github.com/ChaosDevLikan/)
+# Copyright (C) Prydev      (http://sourceforge.net/projects/prydevserver) 
+# Copyright (C) TrinityCore (http://www.trinitycore.org)
+# Copyright (C) MaNGOS      (http://www.getmangos.com)
+# Copyright (C) ArkCORE     (http://www.arkania.net)
+# Copyright (C) SkyFireEMU  (http://www.projectskyfire.org)
+# 
+# World of Warcraft
+# You can edit but you can not delete this copyright.
+# Genesis Emulator - Genesis Database 
+# 
+# 
+#  By Likan Developer Core C++, SQL (FUN, WoTLK, Cata Realm) of Genesis Server
+# 
+
+# This file is run right before CMake starts configuring the sourcetree
+
+# Example: Force CMAKE_INSTALL_PREFIX to be preloaded with something before
+# doing the actual first "configure"-part - allows for hardforcing
+# destinations elsewhere in the CMake buildsystem (commented out on purpose)
+
+# Override CMAKE_INSTALL_PREFIX on Windows platforms
+#if( WIN32 )
+#  if( NOT CYGWIN )
+#    set(CMAKE_INSTALL_PREFIX
+#      "" CACHE PATH "Default install path")
+#  endif()
+#endif()
