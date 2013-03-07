efb95a4524e7047fdc83121bdcc49b2e52d54b9f
Likan <ChaosDev@seznam.cz>
2013-03-07 19:21:47 +0100

    hybrid
    - hybrid all version

--- sql/hybrid/npc_hybrid_0.2.cpp
+++ sql/hybrid/npc_hybrid_0.2.cpp
@@ -1,12 +1,17 @@
 /*
- * Copyright (C) 2009 Gameshoot.eu <http://www.Gameshoot.eu/>
+ * Copyright (C) Likan Mod   (https://github.com/ChaosDevLikan/)
+ * Copyright (C) Prydev      (http://sourceforge.net/projects/prydevserver) 
+ * Copyright (C) TrinityCore (http://www.trinitycore.org)
+ * Copyright (C) MaNGOS      (http://www.getmangos.com)
+ * Copyright (C) ArkCORE     (http://www.arkania.net)
+ * Copyright (C) SkyFireEMU  (http://www.projectskyfire.org)
  *
- * World of Warcraft: Cataclysm 
+ * World of Warcraft
  * You can edit but you can not delete this copyright.
- * SkyFire Emulator - SkyFire Database  
- * Cataclysm Hybrid
+ * Genesis Emulator - Genesis Database 
  *
- * By Likan - Head Admin end Developer of Gameshoot.eu Server
+ *
+ * By Likan Developer Core C++, SQL (FUN, WoTLK, Cata Realm) of Genesis Server
  */
 
 /* ScriptData
